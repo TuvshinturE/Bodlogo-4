f11cd1050b3262bad4dbe8e6cc56e527d39f9dfd
Bayarsaikhan <bayaraa@gmail.com>
2020-01-26 12:39:28 +0800

    git1

---
+++ app.py
@@ -0,0 +1,12 @@
+# variable
+# data types /int, float, str
+# input(), print(), type()
+#function, method, apra
+owog= "Baatar"
+print("urt", len(owog))
+print("0 r temdegt", owog[5])
+print(owog.upper())
+print(owog.lower())
+print("bomb".capitalize())
+print(owog.find("r"))
+print(owog[1:6])
